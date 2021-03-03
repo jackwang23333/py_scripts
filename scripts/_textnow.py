1dca0ecc5dcc9b6b962cab901ba7dd55078cd2d1
jackwang23333 <77010231+jackwang23333@users.noreply.github.com>
2021-03-04 00:03:27 +0800

    Update _textnow.py

--- scripts/_textnow.py
+++ scripts/_textnow.py
@@ -8,6 +8,12 @@ usernames = os.environ["TEXTNOW_USERNAME"].split(',')
 passwords = os.environ["TEXTNOW_PASSWORD"].split(',')
 cookies = os.environ["TEXTNOW_COOKIES"].split('##,##')
 
+
+import datetime
+utc = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+UTC_FORMAT = "%Y-%m-%d %H:%M:%S"
+utc_time = datetime.datetime.strptime(utc, UTC_FORMAT)
+local_time = utc_time + datetime.timedelta(hours=8)
 if len(usernames) != len(passwords) :
   print(u"账号和密码个数不对应")
   quit()
@@ -21,7 +27,7 @@ print(u"共有 %s 个账号，即将开始保号处理" % len(cookies))
 
 numbers = os.environ["TEXTNOW_NUMBER"]
 #msg = os.environ["TEXTNOW_MSG"]
-msg = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+msg = "现在的北京时间是:"+local_time+" random code:"+str(random.randint(100000,999999))
 for idx in range(0,len(usernames)):
   username = usernames[idx]
   password = passwords[idx]
